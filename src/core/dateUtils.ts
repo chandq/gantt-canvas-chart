075c67d2c0c439023b709ccc18e333c4fad6702b
chendq <deqiaochen@foxmail.com>
2025-12-18 09:54:03 +0800

    fix: avoid use RegExp.$1

--- src/core/dateUtils.ts
+++ src/core/dateUtils.ts
@@ -2,24 +2,27 @@ export class DateUtils {
   static readonly ONE_DAY_MS = 24 * 60 * 60 * 1000;
 
   static format(date: Date, format = 'yyyy-MM-dd'): string {
-    const o: Record<string, any> = {
-      'M+': date.getMonth() + 1,
-      'd+': date.getDate(),
-      'h+': date.getHours(),
-      'm+': date.getMinutes(),
-      's+': date.getSeconds(),
+    let fmt = format;
+    let ret;
+    const opt: Record<string, any> = {
+      'Y+': `${date.getFullYear()}`, // 年
+      'y+': `${date.getFullYear()}`, // 年
+      'M+': `${date.getMonth() + 1}`, // 月
+      'D+': `${date.getDate()}`, // 日
+      'd+': `${date.getDate()}`, // 日
+      'h+': `${date.getHours()}`, // 时
+      'm+': `${date.getMinutes()}`, // 分
+      's+': `${date.getSeconds()}`, // 秒
       'q+': Math.floor((date.getMonth() + 3) / 3),
       W: ['日', '一', '二', '三', '四', '五', '六'][date.getDay()]
     };
-    if (/(y+)/.test(format))
-      format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-    for (let k in o)
-      if (new RegExp('(' + k + ')').test(format))
-        format = format.replace(
-          RegExp.$1,
-          RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
-        );
-    return format;
+    for (const k in opt) {
+      ret = new RegExp('(' + k + ')').exec(fmt);
+      if (ret) {
+        fmt = fmt.replace(ret[1], ret[1].length === 1 ? opt[k] : opt[k].padStart(ret[1].length, '0'));
+      }
+    }
+    return fmt;
   }
 
   static addDays(date: Date | string, days: number): Date {
